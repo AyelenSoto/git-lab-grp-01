cb632a1790af6254c5b65a08cf5d1418953bdc81
Alexandra Robles <alexandra.robles.u@tecsup.edu.pe>
2026-04-01 22:19:06 -0500

    feat: se agrega StudentView

---
+++ StudentView.java
@@ -0,0 +1,12 @@
+package pe.edu.tecsup.lab;
+import java.util.List;
+
+public class StudentView {
+    public void displayStudent(List<String> students) {
+        System.out.println("=== LISTADO DE ESTUDIANTES ===");
+        for (String student : students) {
+            System.out.println("Estudiante: " + student);
+        }
+        System.out.println("Total: " + students.size());
+    }
+}
