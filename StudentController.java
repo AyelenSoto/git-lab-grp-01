f26f5819d62c2ca438faaa2b2e575e19d323e7da
AyelenSoto <ayelen.soto@tecsup.edu.pe>
2026-04-01 23:43:18 -0500

    feat(sprint-2): mejora en StudentController.java — nuevo estudiante

---
+++ StudentController.java
@@ -0,0 +1,21 @@
+package pe.edu.tecsup.lab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentController {
+
+    public List<String> getStudents() {
+
+        // Lógica agregada
+        List<String> students = new ArrayList<>();
+
+        students.add("Ayelén Soto");
+        students.add("Willy Torres");
+        students.add("Katherine Quispe"); 
+        students.add("Alexandra Robles");
+        students.add("Julius Novachorno");
+
+        return students;
+    }
+}
