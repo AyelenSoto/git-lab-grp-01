e89b5bbe0450c65e4be6ac0ce0e2cf0e3aca40a5
willytorres3001 <willy.torres@tecsup.edu.pe>
2026-04-01 23:38:39 -0500

    feat(sprint-2): mejora en StudentModel - se agrega atributo email

--- src/main/java/pe/edu/tecsup/lab/StudentModel.java
+++ src/main/java/pe/edu/tecsup/lab/StudentModel.java
@@ -2,6 +2,5 @@ public class StudentModel {
     int id;
     String nombre;
     String apellido;
-
-    // Métodos del modelo agregados en el sprint 1
+    String email; // Nueva mejora del sprint 2
 }
