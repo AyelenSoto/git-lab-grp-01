378c99bca43269de364f5c8639e6fc924a294809
katherinequisper-glitch <katherine.quispe.r@tecsup.edu.pe>
2026-04-01 21:37:14 -0500

    feat: se agrega StudentReport

--- StudentReport.java
+++ StudentReport.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 public class StudentReport {
 
-    // Este método recibe la lista de nombres que creó tu responsable
     public void generateReport(List<String> students) {
         System.out.println("   Reporte de los estudiantes: ");
         
-        // Recorremos la lista para imprimir cada nombre
         for (String student : students) {
             System.out.println("Estudiante registrado: " + student);
         }
